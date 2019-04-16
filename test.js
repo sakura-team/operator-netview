c39aebe72317f98f8a200f1ab00cc7c4f5960ec2
Etienne Dublé <etienne.duble@imag.fr>
2019-04-16 14:35:07 +0200

    Allow dragging the network view.

--- test.js
+++ test.js
@@ -14,15 +14,17 @@ function round2Decimals(f) {
     return Math.round(f * 100) / 100;
 }
 
-function svgStartDrag(evt, node) {
-    let mouse = getMousePosition(evt);
-    let center = node.getCenter();
+function svgStartDrag(evt, elem) {
+    evt.preventDefault();
+    if (dragInfo.elem != null) {
+        // dragging already handled by an element with higher z-index
+        return;
+    }
+    let mousePos = getMousePosition(evt);
+    let info = elem.onStartDrag(mousePos);
     dragInfo = {
-        elem: node,
-        offset: {
-            x: mouse.x - center.x,
-            y: mouse.y - center.y
-        }
+        elem: elem,
+        info: info
     };
     svggraph.addEventListener('mousemove', svgDrag);
     svggraph.addEventListener('mouseup', svgEndDrag);
@@ -31,27 +33,36 @@ function svgStartDrag(evt, node) {
 
 function svgDrag(evt) {
     evt.preventDefault();
-    // move dragged element center
-    let mouse = getMousePosition(evt);
-    dragInfo.elem.setCenter({
-        x: round2Decimals(mouse.x - dragInfo.offset.x),
-        y: round2Decimals(mouse.y - dragInfo.offset.y),
-    });
     // call element's custom callback
-    dragInfo.elem.onDrag();
+    let mousePos = getMousePosition(evt);
+    dragInfo.elem.onDrag(dragInfo.info, mousePos);
 }
 
 function svgEndDrag() {
+    dragInfo.elem.onEndDrag(dragInfo.info);
+    dragInfo.elem = null;
     svggraph.removeEventListener('mousemove', svgDrag);
     svggraph.removeEventListener('mouseup', svgEndDrag);
     svggraph.removeEventListener('mouseleave', svgEndDrag);
 }
 
-function getMousePosition(evt) {
-    let CTM = svggraph.getScreenCTM();
+function pixelsToSvg(ctm, x, y) {
+    if (ctm == null) {
+        ctm = svggraph.getScreenCTM();
+    }
     return {
-        x: (evt.clientX - CTM.e) / CTM.a,
-        y: (evt.clientY - CTM.f) / CTM.d
+        x: (x - ctm.e) / ctm.a,
+        y: (y - ctm.f) / ctm.d
+    }
+}
+
+function getMousePosition(evt) {
+    return {
+        pixels: {
+            x: evt.clientX,
+            y: evt.clientY
+        },
+        svg: pixelsToSvg(null, evt.clientX, evt.clientY)
     };
 }
 
@@ -74,11 +85,32 @@ function Node(x, y, label) {
         svg.appendChild(circle);
         svg.appendChild(text);
     }
-    this.onDrag = function(evt) {
+    this.onStartDrag = function(mousePos) {
+        let center = this.getCenter();
+        let offset = {
+            x: mousePos.svg.x - center.x,
+            y: mousePos.svg.y - center.y
+        };
+        return { offset: offset };
+    };
+    this.onDrag = function(info, mousePos) {
+        // move center
+        this.setCenter({
+            x: round2Decimals(mousePos.svg.x - info.offset.x),
+            y: round2Decimals(mousePos.svg.y - info.offset.y),
+        });
         // redraw linked arrows
         this.linkedArrows.forEach(function(arrow) {
             arrow.redraw();
         });
+    };
+    this.onEndDrag = function(info) {
+    };
+    this.getCenter = function() {
+        return {
+            x: parseInt(this.circles[0].getAttributeNS(null, 'cx'), 10),
+            y: parseInt(this.circles[0].getAttributeNS(null, 'cy'), 10)
+        };
     };
     this.setCenter = function(c) {
         for (circle of this.circles) {
@@ -90,14 +122,70 @@ function Node(x, y, label) {
             text.setAttributeNS(null, 'y', c.y + LABEL_OFFSET_Y);
         }
     };
-    this.getCenter = function() {
-        return {
-            x: parseInt(this.circles[0].getAttributeNS(null, 'cx'), 10),
-            y: parseInt(this.circles[0].getAttributeNS(null, 'cy'), 10)
-        };
-    };
     this.setCenter({x: x, y: y});
     return this;
+}
+
+function NetGraph() {
+    this.viewbox = null;
+    this.getViewBox = function() {
+        return this.viewbox;
+    };
+    this.setViewBox = function(x, y, width, height) {
+        this.viewbox = { x: x, y: y, width: width, height: height };
+        svggraph.setAttributeNS(null, "viewBox",
+                    x + " " + y + " " + width + " " + height);
+    };
+    this.onStartDrag = function(mousePos) {
+        svggraph.classList.add("dragging");
+        return { mouseStart:    mousePos.svg,
+                 ctm:           svggraph.getScreenCTM(),
+                 viewbox:       this.viewbox };
+    };
+    this.onDrag = function(info, mousePos) {
+        // compute mouse offset
+        let mouseEnd = pixelsToSvg( info.ctm,
+                                    mousePos.pixels.x,
+                                    mousePos.pixels.y);
+        let offset = {  x: mouseEnd.x - info.mouseStart.x,
+                        y: mouseEnd.y - info.mouseStart.y };
+        // translate viewbox
+        this.setViewBox(round2Decimals(info.viewbox.x - offset.x),
+                        round2Decimals(info.viewbox.y - offset.y),
+                        info.viewbox.width,
+                        info.viewbox.height);
+    };
+    this.onEndDrag = function(info) {
+        svggraph.classList.remove("dragging");
+    };
+    this.resize = function(maxX, maxY) {
+        let div = document.querySelector("#leftrow");
+        let component_w = div.scrollWidth;
+        let component_h = div.scrollHeight;
+        let viewbox_w, viewbox_h;
+        if (maxX/component_w > maxY/component_h) {
+            // compute size based on width
+            viewbox_w = maxX;
+            viewbox_h = Math.floor((maxX * component_h) / component_w);
+        }
+        else {
+            // compute size based on height
+            viewbox_h = maxY;
+            viewbox_w = Math.floor((maxY * component_w) / component_h);
+        }
+        this.setViewBox(0, 0, viewbox_w, viewbox_h);
+        svggraph.setAttributeNS(null, "width", component_w);
+        svggraph.setAttributeNS(null, "height", component_h);
+    };
+    let saved_this = this;
+    svggraph.addEventListener('mousedown', function(evt) { svgStartDrag(evt, saved_this); });
+    this.resize(500, 600);
+}
+
+function initNetGraph() {
+    svggraph = document.querySelector("#svggraph");
+    netGraph = new NetGraph();
+    initZoomControls();
 }
 
 function Arrow(node1, node2) {
@@ -143,26 +231,6 @@ function Arrow(node1, node2) {
     return this;
 }
 
-function sizeSvg(maxX, maxY) {
-    let div = document.querySelector("#leftrow");
-    let component_w = div.scrollWidth;
-    let component_h = div.scrollHeight;
-    let viewbox_w, viewbox_h;
-    if (maxX/component_w > maxY/component_h) {
-        // compute size based on width
-        viewbox_w = maxX;
-        viewbox_h = Math.floor((maxX * component_h) / component_w);
-    }
-    else {
-        // compute size based on height
-        viewbox_h = maxY;
-        viewbox_w = Math.floor((maxY * component_w) / component_h);
-    }
-    svggraph.setAttributeNS(null, "viewBox", "0 0 " + viewbox_w + " " + viewbox_h);
-    svggraph.setAttributeNS(null, "width", component_w);
-    svggraph.setAttributeNS(null, "height", component_h);
-}
-
 function initZoomControls() {
     /* copy main svg content to small one of zoom controls */
     let svgContent = document.querySelector("#svgdiv").innerHTML;
@@ -179,9 +247,7 @@ function initZoomControls() {
 }
 
 function initJs() {
-    svggraph = document.querySelector("#svggraph");
-    sizeSvg(500, 600);
-    initZoomControls();
+    initNetGraph();
 
     let n1 = new Node(250, 250, "N1");
     let n2 = new Node(350, 350, "N2");
